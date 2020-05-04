2f1798a360b45f54b06e4642e9c0fb9c9b642716
LiefTheBeef <32721304+LiefTheBeef@users.noreply.github.com>
2020-05-04 16:35:51 -0700

    Added UI/UX zip files, played around with basic React App code

--- Frontend/web/src/App.js
+++ Frontend/web/src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/56673182_1566539410150329_8110857348345495552_n.png?_nc_cat=109&_nc_sid=85a577&_nc_ohc=ffcnhN9XOdsAX8wi43i&_nc_ht=scontent-sjc3-1.xx&oh=90a7a2f54ccd4c5088c2920155b4d935&oe=5ED808A0" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/fusionuci/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fusion Facebook Page
         </a>
       </header>
     </div>
